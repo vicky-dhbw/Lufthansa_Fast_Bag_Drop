bbded61b42be91191df9caece8cca94f1f7a5473
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-10-25 01:27:13 +0200

    check in contd. scan done, boarding pass handed over to passenger voucher issued

---
+++ src/main/java/services/QRCodeGenerator.java
@@ -0,0 +1,11 @@
+package services;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+public class QRCodeGenerator {
+
+}
