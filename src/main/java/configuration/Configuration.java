78a64456e5f0a9e00df410b849e2d1c3e37266cb
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-10-25 22:37:54 +0200

    record contd

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -1,5 +1,8 @@
 package configuration;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public enum Configuration {
     INSTANCE;
 
@@ -9,10 +12,15 @@ public enum Configuration {
     public final String baggageContents="src"+fileSeparator+"main"+fileSeparator+"java"+fileSeparator+"Data"+fileSeparator+"baggage_content.txt";
     public final String baggageWeights="src"+fileSeparator+"main"+fileSeparator+"java"+fileSeparator+"Data"+fileSeparator+"baggage_weight.txt";
     public final String assignmentFile="src"+fileSeparator+"main"+fileSeparator+"java"+fileSeparator+"Data"+fileSeparator+"assignment.csv";
-    public final String qrCodeFile="src"+fileSeparator+"main"+fileSeparator+"java"+fileSeparator+"baggageTags"+fileSeparator+"baggageTag.jpg";
+    //public final String qrCodeFile="src"+fileSeparator+"main"+fileSeparator+"java"+fileSeparator+"baggageTags"+fileSeparator+"baggageTag.jpg";
 
     //public final String getQrCodeFile="C:"+fileSeparator+fileSeparator+"TAG"+fileSeparator+"baggageTag.jpg";
+    final Path path= Paths.get("baggageTag.jpg");
+    public final String qrCodeFile= String.valueOf(path.toAbsolutePath());
 
+
+    public final String qrCodeFile1="./baggageTags/baggageTag.jpg";
+    public final String qrCodeFile2="";
     public final String dataPath = "data" + fileSeparator;
 
 }
