fdbd8106f84b702b1e6ab9df2f6a2825fd5a10dc
vicky-dhbw <101749450+vicky-dhbw@users.noreply.github.com>
2022-10-25 12:43:00 +0200

    check in contd. scan done, boarding pass handed over to passenger voucher issued

--- src/main/java/services/QRCodeGenerator.java
+++ src/main/java/services/QRCodeGenerator.java
@@ -1,11 +1,31 @@
 package services;
 
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.*;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.client.j2se
+import flightRelevants.FlightID;
+import flightRelevants.IATAAirportCodes;
 
 public class QRCodeGenerator {
+
+    public void generateQRCode() throws WriterException {
+        try{
+            String data="Hallo Vicky";
+            String path= IATAAirportCodes.FRA.toString()+" "+IATAAirportCodes.HKG.toString()+" "+ FlightID.LH2121.toString();
+
+            BitMatrix matrix =new MultiFormatWriter().encode(data,BarcodeFormat.QR_CODE,500,500);
+            MatrixToImageWriter.write(matrix,"jpg",path);
+        } catch(Exception e){
+
+        }
+    }
+
+
+
+
 
 }
